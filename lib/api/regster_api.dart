b631f5f7a84116c203bb94fb5c04e34d22540e43
DhamotharanE0903 <120079275+DhamotharanE0903@users.noreply.github.com>
2023-03-07 17:47:51 +0530

    paymentviewall page

--- lib/api/regster_api.dart
+++ lib/api/regster_api.dart
@@ -687,6 +687,7 @@ hideLoading();
   //termsandconditions post
 
   void Termspost() async {
+    showLoading();
     DateTime datetime = DateTime.now();
     String dateaccpt = datetime.toString();
     var customerids = GetStorage().read("custid");
